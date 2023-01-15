8cabf0bec72fda5a0fffe46996fb69a268be0a8d
Benji Kehat <93135052+ZBenjiKe@users.noreply.github.com>
2023-01-15 16:04:57 +0200

    Add files via upload

--- Assignment 2/MyThreadPoolExecutor.java
+++ Assignment 2/MyThreadPoolExecutor.java
@@ -8,69 +8,64 @@ import java.util.concurrent.TimeUnit;
 
 public class MyThreadPoolExecutor extends ThreadPoolExecutor{
 
-	static int processors = Runtime.getRuntime().availableProcessors();
-	private int [] priorityArray = new int [11];
-	private int maxPriority = 100;
-	
-	public MyThreadPoolExecutor() {
-		super(processors/2, processors-1, 300, TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>());		
-	}
-	
-	protected <V> RunnableFuture<V> newTaskFor(Task<V> task) {
-		CustomFutureTask<V> ft = new CustomFutureTask<>(task);
-		return ft;
-	}
-	
+    static int processors = Runtime.getRuntime().availableProcessors();
+    private int [] priorityArray = new int [11];
+    private int maxPriority = 100;
+
+    public MyThreadPoolExecutor() {
+        super(processors/2, processors-1, 300, TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>());
+    }
+
+    protected <V> RunnableFuture<V> newTaskFor(Task<V> task) {
+        CustomFutureTask<V> ft = new CustomFutureTask<>(task);
+        return ft;
+    }
+
     public <V> Future<V> submit(Task<V> task) {
-        if (task == null) throw new NullPointerException();
-        RunnableFuture<V> ftask = newTaskFor(task);        
+        if (task == null)
+            throw new NullPointerException();
+        RunnableFuture<V> ftask = newTaskFor(task);
+        int priority = task.getTaskPriority();
+        priorityArray[priority]++;
+        if(priority < maxPriority) {
+            maxPriority = priority;
+        }
+
         execute(ftask);
         return ftask;
     }
-	
+
     @Override
-    protected void beforeExecute(Thread thread, Runnable run) {     	
-    	if (run instanceof CustomFutureTask) {
-	    	CustomFutureTask cft = (CustomFutureTask)run;
-	    	int priority = cft.getTask().getTaskPriority();
-	    	priorityArray[priority]++;
-	    	if(priority < maxPriority) {
-	    		maxPriority = priority;
-	    	}
-    	}
+    protected void beforeExecute(Thread thread, Runnable run) {
+        if (run instanceof CustomFutureTask) {
+            CustomFutureTask<?> cft = (CustomFutureTask<?>)run;
+            int priority = cft.getTask().getTaskPriority();
+            priorityArray[priority]--;
+            if(priority == maxPriority && priorityArray[priority] == 0) {
+                int i = priority+1;
+                while(i < 11 && priorityArray[i] == 0) {
+                    i++;
+                }
+                maxPriority = i;
+            }
+        }
         super.beforeExecute(thread, run);
     }
-    
-    @Override
-    protected void afterExecute(Runnable r, Throwable t) {
-    	super.afterExecute(r, t);
-    	if (r instanceof CustomFutureTask) {
-	    	CustomFutureTask cft = (CustomFutureTask)r;
-	    	int priority = cft.getTask().getTaskPriority();
-	    	priorityArray[priority]--;
-	    	if(priority == maxPriority && priorityArray[priority] == 0) {
-	    		int i = priority+1;
-	    		while(i < 11 && priorityArray[i] == 0) {
-	    			i++;
-	    		}
-	    		maxPriority = i;
-	    	}
-    	}
-    }
-    
+
+
     public int getMaxPriority() {
-    	return maxPriority;
+        return maxPriority;
     }
-    
-	public void gracefullyTerminate() {
-	    this.shutdown();
-	    try {
-	        if (!this.awaitTermination(60, TimeUnit.SECONDS)) {
-	            this.shutdownNow();
-	        }
-	    } catch (InterruptedException ex) {
-	        this.shutdownNow();
-	        Thread.currentThread().interrupt();
-	    }
-	}
+
+    public void gracefullyTerminate() {
+        this.shutdown();
+        try {
+            if (!this.awaitTermination(60, TimeUnit.SECONDS)) {
+                this.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            this.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
 }
